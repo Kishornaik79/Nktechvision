fd7cb8d88f730f18af4d90b49ceaf14bb08c570d
Kishornaik79 <99606589+Kishornaik79@users.noreply.github.com>
2024-10-13 16:41:52 +0530

    added verbs1.js

---
+++ verbs1.js
@@ -0,0 +1,17 @@
+function verbs1(){
+  window.location.href="/verbs.html";
+}
+
+function prepositions() {
+  // Tab to edit
+  window.location.href="/English_Speaking/prepositions.html";
+}
+
+function vocabulary1(){
+  window.location.href="/English_Speaking/vocabularyindex26.html";
+}
+
+function openPage(){
+      window.location.href="/English_Speaking/vocabularyindex26.html";
+      // Tab to edit
+    }
