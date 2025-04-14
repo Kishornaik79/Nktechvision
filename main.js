177539e8934c354f3a695901ba564bad9693beea
Kishornaik79 <99606589+Kishornaik79@users.noreply.github.com>
2025-04-14 18:49:56 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,39 @@
 function toggleMenu() {
-            var navLinks = document.querySelector('.nav-links');
-            navLinks.classList.toggle('active');
-  
+      let menu = document.getElementById("menu");
+      if (menu.style.left === "0px") {
+        menu.style.left = "-100%"; // Close menu
+      } else {
+        menu.style.left = "0px";
+        
+        // Open menu
+      }
+    }
+    
+     function scrollLeft() {
+       document.querySelector(".navbar").scrollBy({ left: -200, behavior: "smooth" });
+     }
+     
+     function scrollRight() {
+       document.querySelector(".navbar").scrollBy({ left: 100, behavior: "smooth" });
+     }
+     function LearnEnglish(){
+       window.location.href ="view.html";
+     }
+     function LearnHtml(){
+       window.location.href ="https://www.w3schools.com/html/default.asp";
+     }
+     function LearnCss() {
+  window.location.href = "https://www.tpointtech.com/css-tutorial";
 }
+
+function LearnJs() {
+  window.location.href = "https://javascript.info/intro";
+}
+
+function LearnPython() {
+  window.location.href = "https://www.tpointtech.com/python-tutorial";
+}
+
+function LearnJava() {
+  window.location.href = "https://www.w3schools.com/java/default.asp";
+                   }
