dee6a5b3cf76914681b9281d14901d6c7e1a26b9
Kishornaik79 <99606589+Kishornaik79@users.noreply.github.com>
2025-04-22 23:45:38 +0530

    Update main.js

--- main.js
+++ main.js
@@ -44,10 +44,10 @@ function introduction_to_html(){
   window.location.href ="Basic_Html_Tags.html";
 }
 function introduction_to_html(){
-  window.location.href ="Basic_Html_Tags.html";
+  window.location.href ="Introduction_to_HTML.html";
 }
 function basic_html_tags(){
-  window.location.href ="Text_Formatting_Tags.html";
+  window.location.href ="Basic_Html_Tags.html";
 }
 function text_formmating_tags_c3(){
   window.location.href = "Text_Formatting_Tags.html";
